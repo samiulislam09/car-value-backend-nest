75bc59629ceba1424788ac9a3c06a8f605edc5e8
samiulislam09 <samiulislamsawon09@gmail.com>
2023-04-04 00:24:30 +0600

    added services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+type NewType = CreateUserDto;
+
+@Controller('auth')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Post('/signup')
+  createUser(@Body() body: NewType) {
+    return this.userService.create(body.email, body.password);
+  }
+  @Get('/user')
+  getUser() {
+    return this.userService.find();
+  }
+}
